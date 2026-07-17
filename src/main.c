a22eab810c6a0e4de9855837bff9a2fd9eaad414
Bones <terraw1999@proton.me>
2026-07-17 15:00:44 -0600

    Add: Stack trace test

--- src/main.c
+++ src/main.c
@@ -18,8 +18,19 @@ int main(int argc, const char* argv[]) {
 	writeChunk(&chunk, OP_CONSTANT, 3);
 	writeChunk(&chunk, constant, 3);
 
+	// Add many values
+	constant = addConstant(&chunk, 66);
+	writeChunk(&chunk, OP_CONSTANT, 6);
+	writeChunk(&chunk, constant, 6);
+	constant = addConstant(&chunk, 42);
+	writeChunk(&chunk, OP_CONSTANT, 12);
+	writeChunk(&chunk, constant, 12);
+	constant = addConstant(&chunk, 888);
+	writeChunk(&chunk, OP_CONSTANT, 12);
+	writeChunk(&chunk, constant, 12);
+
 	// Return statement
-	writeChunk(&chunk, OP_RETURN, 3);
+	writeChunk(&chunk, OP_RETURN, 12);
 
 	// Interpret code in virtual machine
 	interpret(&chunk);
