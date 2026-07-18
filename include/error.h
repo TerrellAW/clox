76fb7d8d1f833e5ff1e30b293e185ea551550776
Bones <terraw1999@proton.me>
2026-07-18 10:47:06 -0600

    Add: Input and error handling

---
+++ include/error.h
@@ -0,0 +1,29 @@
+/**
+ * Macros to define error codes
+ */
+
+#ifndef CLOX_ERROR_H
+#define CLOX_ERROR_H
+
+/**
+ * Failure to start program
+ * Wrong number of arguments
+ */
+#define START_FAILURE 64
+
+/**
+ * Failure to compile
+ */
+#define COMPILE_FAILURE 65
+
+/**
+ * Failure during runtime
+ */
+#define RUNTIME_FAILURE 70
+
+/**
+ * Failure trying to read a file
+ */
+#define READ_FAILURE 74
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,66 +2,111 @@
  * Main entry point
  */
 
+#include <stdio.h>
+#include <string.h>
+
 #include "../include/common.h"
-#include "../include/debug.h"
+#include "../include/error.h"
 #include "../include/vm.h"
+
+// Runs Read-Eval-Print Loop
+static void repl() {
+	// Can only handle limited amount of characters
+	// TODO: Use a dynamic array
+	char line[1024];
+
+	for (;;) {
+		printf("> ");
+
+		// Exit if EOF registered on stdin
+		if (!fgets(line, sizeof(line), stdin)) {
+			printf("\n");
+			break;
+		}
+
+		// Run interpreter on inputted line
+		interpret(line);
+	}
+}
+
+// Read file and dynamically allocate string for its contents
+static char* readFile(const char* path) {
+	// Open file
+	FILE* file = fopen(path, "rb");
+
+	// Handle failure to open file
+	if (file == NULL) {
+		fprintf(stderr, "Could not open file \"%s\".\n", path);
+		exit(READ_FAILURE);
+	}
+
+	// Get file size
+	fseek(file, 0L, SEEK_END);
+	size_t fileSize = ftell(file);
+	rewind(file);
+
+	// Allocate a memory buffer for file contents with extra space for EOF
+	char* buffer = (char*)malloc(fileSize + 1);
+
+	// Handle not enough memory
+	if (buffer == NULL) {
+		fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+		exit(READ_FAILURE);
+	}
+
+	// Read to end of file
+	size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+
+	// Handle read failure
+	if (bytesRead < fileSize) {
+		fprintf(stderr, "Could not read file \"%s\".\n", path);
+		exit(READ_FAILURE);
+	}
+
+	// Add EOF
+	buffer[bytesRead] = '\0';
+
+	// Close file reader
+	fclose(file);
+
+	// Return buffer
+	return buffer;
+}
+
+// Compiles and runs a file
+static void runFile(const char* path) {
+	// Read source file
+	char* source = readFile(path);
+
+	// Store result from interpreter
+	InterpretResult result = interpret(source);
+
+	// Free memory
+	free(source);
+
+	// Handle error results
+	if (result == INTERPRET_COMPILE_ERROR) exit(COMPILE_FAILURE);
+	if (result == INTERPRET_RUNTIME_ERROR) exit(RUNTIME_FAILURE);
+}
 
 int main(int argc, const char* argv[]) {
 	// Initialize the virtual machine
 	initVM();
 
-	Chunk chunk;
-	initChunk(&chunk);
+	// Process arguments
+	if (argc == 1) {
+		// If no file specified enter REPL
+		repl();
+	} else if (argc == 2) {
+		// If file specified run it
+		runFile(argv[1]);
+	} else {
+		// Give error
+		fprintf(stderr, "Usage: clox [path]\n");
+		exit(START_FAILURE);
+	}
 
-	// Add 1.2 to the stack
-	size_t constant = addConstant(&chunk, 1.2);
-	writeChunk(&chunk, OP_CONSTANT, 3);
-	writeChunk(&chunk, constant, 3);
-
-	// Add 5 to the stack
-	constant = addConstant(&chunk, 5);
-	writeChunk(&chunk, OP_CONSTANT, 4);
-	writeChunk(&chunk, constant, 4);
-
-	// Multiply 1.2 by 5
-	writeChunk(&chunk, OP_MULTIPLY, 5);
-
-	// Add 24 to stack
-	constant = addConstant(&chunk, 24);
-	writeChunk(&chunk, OP_CONSTANT, 6);
-	writeChunk(&chunk, constant, 6);
-
-	// Add 0.4 to stack
-	constant = addConstant(&chunk, 0.4);
-	writeChunk(&chunk, OP_CONSTANT, 10);
-	writeChunk(&chunk, constant, 10);
-
-	// Divide 8 by 0.4	
-	writeChunk(&chunk, OP_DIVIDE, 11);
-
-	// Add 6 to 60
-	writeChunk(&chunk, OP_ADD, 11);
-
-	// Negate 66
-	writeChunk(&chunk, OP_NEGATE, 13);
-
-	// Add 108 to stack
-	constant = addConstant(&chunk, 108);
-	writeChunk(&chunk, OP_CONSTANT, 13);
-	writeChunk(&chunk, constant, 13);
-
-	// Add 108 to -66
-	writeChunk(&chunk, OP_ADD, 13);
-
-	// Return the meaning of life
-	writeChunk(&chunk, OP_RETURN, 13);
-
-	// Interpret code in virtual machine
-	interpret(&chunk);
-
-	// Free the virtual machine
+	// Free the virtual machine and exit
 	freeVM();
-
-	freeChunk(&chunk);
 	return EXIT_SUCCESS;
 }
