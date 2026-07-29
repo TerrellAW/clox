e7fc1324922dcbace0fcbc4241fed451dc2ff164
Bones <terraw1999@proton.me>
2026-07-29 10:11:35 -0600

    Add: Heap allocated string type handling

---
+++ include/object.h
@@ -0,0 +1,58 @@
+/**
+ * Heap allocated data object
+ */
+#ifndef CLOX_OBJECT_H
+#define CLOX_OBJECT_H
+
+#include "common.h"
+#include "value.h"
+
+/**
+ * Macro to extract an object type from a Value
+ */
+#define OBJ_TYPE(value)		(AS_OBJ(value)->type)
+
+/**
+ * Macro to check if object is a string
+ */
+#define IS_STRING(value)	isObjType(value, OBJ_STRING)
+
+/**
+ * Macro to return ObjString pointer from Value
+ */
+#define AS_STRING(value)	((ObjString*)AS_OBJ(value))
+
+/**
+ * Macro to return a character array from a Value
+ */
+#define AS_CSTRING(value)	(((ObjString*)AS_OBJ(value))->chars)
+
+/**
+ * Valid heap allocated data types
+ */
+typedef enum {
+	OBJ_STRING,
+} ObjType;
+
+/**
+ * Heap allocated object struct
+ */
+struct Obj {
+	ObjType type;
+};
+
+/**
+ * String type heap allocated data object
+ */
+struct ObjString {
+	Obj obj;
+	size_t length;
+	char* chars;
+};
+
+// Check data type of an object
+static inline bool isObjType(Value value, ObjType type) {
+	return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
--- include/value.h
+++ include/value.h
@@ -8,12 +8,23 @@
 #include "common.h"
 
 /**
+ * Heap allocated data object
+ */
+typedef struct Obj Obj;
+
+/**
+ * String type heap allocated data object
+ */
+typedef struct ObjString ObjString;
+
+/**
  * Enum of valid data types
  */
 typedef enum {
 	VAL_BOOL,
 	VAL_NIL,
 	VAL_NUMBER,
+	VAL_OBJ
 } ValueType;
 
 /**
@@ -24,6 +35,8 @@ typedef struct {
 	union {
 		bool boolean;
 		double number;
+		// Heap allocated data
+		Obj* obj;
 	} as;
 } Value;
 
@@ -33,12 +46,14 @@ typedef struct {
 #define IS_BOOL(value)		((value).type == VAL_BOOL)
 #define IS_NIL(value)		((value).type == VAL_NIL)
 #define IS_NUMBER(value)	((value).type == VAL_NUMBER)
+#define IS_OBJ(value)		((value).type == VAL_OBJ)
 
 /**
  * Value data type unwrapping macros
  */
 #define AS_BOOL(value)		((value).as.boolean)
 #define AS_NUMBER(value)	((value).as.number)
+#define AS_OBJ(value)		((value).as.obj)
 
 /**
  * Value data type casting macros
@@ -46,6 +61,7 @@ typedef struct {
 #define BOOL_VAL(value)		((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL				((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value)	((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object)		((Value){VAL_OBJ, {.obj = (Obj*)object}})
 
 /**
  * Dynamic array of Values
